e1d3da0096de13578fa3d59492141bd89ba9446b
neto1920 <neto.webprojetos@gmail.com>
2022-02-06 12:57:30 -0300

    Completed Project

--- criar_materia.php
+++ criar_materia.php
@@ -57,13 +57,13 @@
                   <form method="post" action="adiciona_materia.php">
                     <div class="form-group">
                       <label>Título</label>
-                      <input name="titulo" type="text" class="form-control" placeholder="Título">
+                      <input name="titulo" type="text" class="form-control" placeholder="Título" required>
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select name="categoria" class="form-control">
-                        <option>...</option>
+                      <select name="categoria" class="form-control" required>
+                        <option> </option>
                         <option>Politica</option>
                         <option>Turismo</option>
                         <option>Moda</option>
@@ -74,13 +74,14 @@
                     
                     <div class="mb-3">
                       <label>Descrição</label>
-                      <textarea name="descricao" class="form-control" rows="3"></textarea>
+                      <textarea name="descricao" class="form-control" rows="3" required ></textarea>
                     </div>
                     <div class="row mt-5">
                       <div class="col-6">
                         <a class="btn btn-lg btn-warning btn-block" type="button" href="home.php">Cancelar</a>
                       </div>
                       <div class="col-6 d-grid justify-content-end">
+                        
                         <button class="btn btn-lg btn-info btn-block " type="submit">Publicar Matéria</button>
                       </div>
                     </div>
